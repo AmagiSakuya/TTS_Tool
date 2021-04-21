1f6693e1c4b73d238eb03163669846ed2668df86
AmagiSakuya <AmagiSakuya@163.com>
2021-04-21 14:59:22 +0800

    first commit

---
+++ TTS_Tool/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace TTS_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(392, 231);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(8, 8);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(43, 148);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(297, 54);
+            this.SubmitButton.TabIndex = 1;
+            this.SubmitButton.Text = "开始";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(43, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(297, 117);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "使用说明：\r\n第一步：点击【开始】按钮\r\n第二步：选择需要转换的【文本】\r\n第三步：选择输出【文件夹】\r\n第四步：等待完成";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 234);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "语音生成工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TTS_Tool/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace TTS_Tool
+{
+
+    public partial class Form1 : Form
+    {
+        string settingsPath = "./Settings.json"; //配置文件路径
+        string dicPath = "./Dictionary.json"; //字典路径
+
+        SpeechSynthesizer m_speaker;
+        Settings m_settings;
+        WordConvertionItems m_dic;
+
+        // 检查配置文件是否存在
+        bool CheckSettings()
+        {
+            if (!File.Exists(settingsPath)) return false;
+            if (!File.Exists(dicPath)) return false;
+            return true;
+        }
+
+        // 读取配置文件
+        bool LoadSettings(out Settings settings, out WordConvertionItems dic)
+        {
+            string settingsStr = File.ReadAllText(settingsPath);
+            string dicStr = File.ReadAllText(dicPath);
+            try
+            {
+                settings = JsonConvert.DeserializeObject<Settings>(settingsStr);
+                dic = JsonConvert.DeserializeObject<WordConvertionItems>(dicStr);
+                return true;
+            }
+            catch (Exception e)
+            {
+                settings = null;
+                dic = null;
+                return false;
+            }
+        }
+
+        bool CheckVoicer(SpeechSynthesizer speaker, string voicerName)
+        {
+            var voices = speaker.GetInstalledVoices();
+            for (int i = 0; i < voices.Count; i++)
+            {
+                if (voices[i].VoiceInfo.Name.Equals(voicerName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool CheckSpeakReady(out SpeechSynthesizer speaker, out Settings settings, out WordConvertionItems dic, out string errorMsg)
+        {
+            speaker = new SpeechSynthesizer();
+            errorMsg = "";
+            settings = null;
+            dic = null;
+            if (!CheckSettings())
+            {
+                errorMsg = "读取配置文件错误";
+                return false;
+            }
+
+            if (!LoadSettings(out settings, out dic))
+            {
+                errorMsg = "读取配置文件错误";
+                return false;
+            }
+
+            if (!CheckVoicer(speaker, settings.voicerName))
+            {
+                errorMsg = $"未能获取到语音者{settings.voicerName}";
+                return false;
+            }
+            return true;
+        }
+
+        bool InitSpeaker(out SpeechSynthesizer speaker, out Settings settings, out WordConvertionItems dic, out string errorMsg)
+        {
+            if (CheckSpeakReady(out speaker, out settings, out dic, out errorMsg))
+            {
+                speaker.SelectVoice(settings.voicerName);
+                if (settings.SpeakRate > 10) settings.SpeakRate = 10;
+                if (settings.SpeakRate < -10) settings.SpeakRate = -10;
+                speaker.Rate = settings.SpeakRate;
+                speaker.Volume = settings.speakVolume;
+                return true;
+            }
+            return false;
+        }
+
+        string ReplaceWorldWithDic(string msg)
+        {
+            string result = msg;
+            if (m_dic != null && !string.IsNullOrEmpty(msg))
+            {
+                for (int i = 0; i < m_dic.dictionary.Length; i++)
+                {
+                    result = msg.Replace(m_dic.dictionary[i].origin, m_dic.dictionary[i].output);
+                }
+            }
+            return result;
+        }
+
+        string[] ReplaceWorldWithDic(string[] msgs)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < msgs.Length; i++)
+            {
+                result.Add(ReplaceWorldWithDic(msgs[i]));
+            }
+            return result.ToArray();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length != 3 || !(args[1].Equals("-speak"))) return;
+
+            //enter cmd mode
+            string errorMsg;
+            if (InitSpeaker(out m_speaker, out m_settings, out m_dic, out errorMsg))
+            {
+                m_speaker.Speak(ReplaceWorldWithDic(args[2]));
+                m_speaker.Dispose();
+            }
+            else
+            {
+                Console.WriteLine(errorMsg);
+            }
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //点击【开始】
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string errorMsg;
+            if (!InitSpeaker(out m_speaker, out m_settings, out m_dic, out errorMsg))
+            {
+                MessageBox.Show(errorMsg, "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "文本文档|*.txt";
+            file.ShowDialog();
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择保存路径";
+            dialog.ShowDialog();
+            Console.WriteLine(dialog.SelectedPath);
+
+            string finalPath = $"{dialog.SelectedPath}/Output";
+            if (!Directory.Exists(finalPath))//如果不存在就创建file文件夹
+            {
+                Directory.CreateDirectory(finalPath);
+            }
+
+            string m_text = File.ReadAllText(file.FileName);
+            string[] m_texts = m_text.Split(Environment.NewLine.ToCharArray());
+
+            ReplaceWorldWithDic(m_texts);
+            if (m_dic != null)
+            {
+                for (int i = 0; i < m_texts.Length; i++)
+                {
+                    if (string.IsNullOrEmpty(m_texts[i])) continue;
+                    m_speaker.SetOutputToWaveFile($"{finalPath}/{m_texts[i]}.wav");
+                    m_texts[i] = ReplaceWorldWithDic(m_texts[i]);
+                    m_speaker.Speak(m_texts[i]);
+                }
+            }
+            m_speaker.SetOutputToNull();
+            m_speaker.Dispose();
+            MessageBox.Show("成功", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Process.Start(dialog.SelectedPath);
+        }
+
+    }
+    public class Settings
+    {
+        public string voicerName;
+        public int speakVolume;
+        public int SpeakRate;
+    }
+    public class WordConvertionItem
+    {
+        public string origin;
+        public string output;
+    }
+    public class WordConvertionItems
+    {
+        public WordConvertionItem[] dictionary;
+    }
+}
